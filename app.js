98ba290ba1426cfbe16db6a8565df48baa3372e8
wishinghand <wishinghand@gmail.com>
2016-08-06 09:59:12 -0700

    fixed a tag bug

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var resource = 'topSpots.json';
 var output = $('#output');
 $.getJSON(resource, function(data){
     $.each(data, function(index){
-        $('#tableHead').append("<tr><td>" + data[index].name +"</td><td>" + data[index].description +"</td><td>" + data[index].location +"</td></tr>");
+        $('#tableHead').append("<tr><td>" + data[index].name +"</td><td>" + data[index].description + "</td><td><a href='https://www.google.com/maps?q=" + data[index].location + "'>" + data[index].location + "</a></td></tr>");
     })
 });
