866484cdcc4d8bf1d2728dca937b85e006a17921
wishinghand <wishinghand@gmail.com>
2016-08-07 20:10:05 -0700

    added comments

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+//data stored in variable in case we want to reference it later
 var resource = 'topSpots.json';
-var output = $('#output');
-$.getJSON(resource, function(data){
-    $.each(data, function(index){
-        $('#tableHead').append("<tr><td>" + data[index].name +"</td><td>" + data[index].description + "</td><td><a href='https://www.google.com/maps?q=" + data[index].location + "'>" + data[index].location + "</a></td></tr>");
-    })
+
+$( document ).ready(function() {
+    //jquery method to grab data from resource
+    $.getJSON(resource, function(data){
+        //iterate over each data bit, show it on table in index.html
+        $.each(data, function(index){
+            //super long concatenation- I'd rather build it all in one go than +=. Babel and template strings would make it easier to parse as a reader
+            $('#tableBody').append("<tr><td>" + data[index].name +"</td><td>" + data[index].description + "</td><td class='mapLink'><button class='pure-button'><a href='https://www.google.com/maps?q=" + data[index].location + "'>Click to Open in Google Maps</a></button></td></tr>");
+        })
+    });
 });
